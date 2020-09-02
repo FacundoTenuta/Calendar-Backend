438ed1bbf87a918aa9a84745d95a08041d73fc4f
Facundo Tenuta <facuargush@gmail.com>
2020-09-02 15:52:48 -0300

    v1 CalendarApp

---
+++ public/precache-manifest.a6f1b08d812bd2ea3f5b0b7c471b70e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b196e40e8fb9770e53e6bdf1cd0ead5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7748c6708b61d50e29b5",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "4aa7bab1737fe9bf0dff",
+    "url": "/static/css/main.49eb93df.chunk.css"
+  },
+  {
+    "revision": "7748c6708b61d50e29b5",
+    "url": "/static/js/2.9d41d23f.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.9d41d23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa7bab1737fe9bf0dff",
+    "url": "/static/js/main.8f45c90a.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
